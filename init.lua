be62fb6874a4e4541a8b6f2584dfa7e2522890d6
msbrandt00 <msbrandt00@gmail.com>
2023-12-13 23:02:42 -0600

    Add max text width

--- init.lua
+++ init.lua
@@ -2,13 +2,19 @@ require "michael.lazy"
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.wrap = true
+vim.opt.textwidth = 90
 vim.opt.ignorecase = true
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = false
 vim.opt.tabstop = 1
 -- Press Tab to save and switch to the next buffer
-vim.api.nvim_set_keymap("n", "<Tab>", [[:if &modifiable && !&readonly && &modified | write | endif | bnext<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap(
+  "n",
+  "<Tab>",
+  [[:if &modifiable && !&readonly && &modified | write | endif | bnext<CR>]],
+  { noremap = true, silent = true }
+)
 
 -- Press Shift+Tab to save and switch to the previous buffer
 vim.api.nvim_set_keymap(
