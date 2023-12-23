f56748b23eb2d7c770ffeb9876e08249e8c5ffe1
Michael Brandt <michael.brandt@rockies.com>
2023-12-23 09:31:18 -0700

    reset cache

--- lua/michael/plugins/leetcode.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "ianding1/leetcode.vim",
-  config = function()
-    vim.g.leetcode_default_language = "python3"
-    vim.g.leetcode_browser = "chrome"
-    vim.g.leetcode_solution_filetype = "python3"
-  end,
-}
